9100f21fe44fa9b0109491f757bf8bd5193f9447
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2023-03-14 20:08:21 -0600

    Update prodAx_paralelo.c

--- producto/prodAx_paralelo.c
+++ producto/prodAx_paralelo.c
@@ -1,24 +1,4 @@
-//--------------------------------------------------------------
-// prodAx.c
-//--------------------------------------------------------------
-// Calcula el producto de una matriz m x n por un vector tamaño n
-//--------------------------------------------------------------
-// Auth.  JJCelada - Universidad del Valle de Guatemala
-// Date   2021-10-06
-// Vers.  1.0
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
-
-
- void prodAx(int m, int n, double * restrict A, double * restrict x,
-   double * restrict b);
-
- int main(int argc, char *argv[]) {
-   double *A,*x,*b;
-   int i, j, m, n;
 
    printf("Ingrese las dimensiones m y n de la matriz: ");
    scanf("%d %d",&m,&n);
@@ -46,7 +26,11 @@
       A[i*n+j] = rand()%13+1;
 
   printf("Calculando el producto Ax para m = %d n = %d\n",m,n);
+  clock_t t;
+  t = clock();
   (void) prodAx(m, n, A, x, b);
+  t = clock() - t;
+  double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
 
   printf("\nb: \n");
   for(j=0; j<n; j++)
@@ -54,6 +38,7 @@
   printf("\n\n");
 
   free(A);free(x);free(b);
+  printf("prodAx() took %f seconds to execute \n", time_taken);
 
   return(0);
  }
