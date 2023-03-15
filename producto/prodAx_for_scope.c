cf044f158a564fbbd46beb38a0f998aacabace1a
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2023-03-14 20:07:47 -0600

    Update prodAx_for_scope.c

--- producto/prodAx_for_scope.c
+++ producto/prodAx_for_scope.c
@@ -1,4 +1,26 @@
+//--------------------------------------------------------------
+// prodAx.c
+//--------------------------------------------------------------
+// Calcula el producto de una matriz m x n por un vector tamaño n
+//--------------------------------------------------------------
+// Auth.  JJCelada - Universidad del Valle de Guatemala
+// Date   2021-10-06
+// Vers.  1.0
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+ void prodAx(int m, int n, double * restrict A, double * restrict x,
+   double * restrict b);
+
+ int main(int argc, char *argv[]) {
+   double *A,*x,*b;
+   int i, j, m, n;
+
+   printf("Ingrese las dimensiones m y n de la matriz: ");
    scanf("%d %d",&m,&n);
 
    //---- Asignación de memoria para la matriz A ----
@@ -24,7 +46,11 @@
       A[i*n+j] = rand()%13+1;
 
   printf("Calculando el producto Ax para m = %d n = %d\n",m,n);
+  clock_t t;
+  t = clock();
   (void) prodAx(m, n, A, x, b);
+  t = clock() - t;
+  double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
 
   printf("\nb: \n");
   for(j=0; j<n; j++)
@@ -32,6 +58,7 @@
   printf("\n\n");
 
   free(A);free(x);free(b);
+  printf("prodAx() took %f seconds to execute \n", time_taken);
 
   return(0);
  }
